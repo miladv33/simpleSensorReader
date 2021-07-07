57780352a80d031f7010475931095548a7a390e0
m.mosavi <milad.mousavi.developer@gmail.com>
2021-07-07 21:22:20 +0430

    DirectionFinder works.

--- app/src/main/java/com/kavinduchamiran/sensorreader/DirectionFinder.kt
+++ app/src/main/java/com/kavinduchamiran/sensorreader/DirectionFinder.kt
@@ -23,53 +23,33 @@ interface DirectionFinder : SensorEventListener {
         get() = FloatArray(3)
         set(value) {}
     var mSensorManager: SensorManager
+    var mAccelerometer: Sensor?
+    var mMagnet: Sensor?
     var mLight: Sensor
     var mGyroscope: Sensor
     var resume: Boolean
-    fun startDirectionFinder(context: Context, sensorEventListener: SensorEventListener) {
-        mSensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
-            mSensorManager.registerListener(
-                sensorEventListener,
-                accelerometer,
-                SensorManager.SENSOR_DELAY_NORMAL,
-                SensorManager.SENSOR_DELAY_UI
-            )
-        }
-        mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
-            mSensorManager.registerListener(
-                sensorEventListener,
-                magneticField,
-                SensorManager.SENSOR_DELAY_NORMAL,
-                SensorManager.SENSOR_DELAY_UI
-            )
-        }
-        mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
-        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
-    }
+
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         print("accuracy changed")
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if (event != null && resume) {
+        if (event != null ) {
             if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
 //                findViewById<TextView>(R.id.acc_X).text = event.values[0].toString()
 //                findViewById<TextView>(R.id.acc_Y).text = event.values[1].toString()
 //                findViewById<TextView>(R.id.acc_Z).text = event.values[2].toString()
-                System.arraycopy(
-                    event.values,
-                    0,
-                    accelerometerReading,
-                    0,
-                    accelerometerReading.size
-                )
+                Log.i("miladTestInterface", "onSensorChanged: X = ${event.values[0]}")
+                Log.i("miladTestInterface", "onSensorChanged: Y = ${event.values[1]}")
+                Log.i("miladTestInterface", "onSensorChanged: Z = ${event.values[2]}")
+                System.arraycopy(event.values,0,accelerometerReading,0, accelerometerReading.size)
             }
             if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
                 System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
             }
             if (event.sensor.type == Sensor.TYPE_LIGHT) {
+                Log.i("miladTestInterface", "onSensorChanged: light = ${event.values[0]}")
 //                findViewById<TextView>(R.id.light).text = event.values[0].toString()
             }
 
@@ -80,11 +60,20 @@ interface DirectionFinder : SensorEventListener {
 //                    ((Math.toDegrees(event.values[1].toDouble()) + 360) % 360).toString()
 //                findViewById<TextView>(R.id.gyro_z).text =
 //                    ((Math.toDegrees(event.values[2].toDouble()) + 360) % 360).toString()
+                Log.i("miladTestInterface", "onSensorChanged: x = ${ ((Math.toDegrees(event.values[0].toDouble()) + 360) % 360)}")
+                Log.i("miladTestInterface", "onSensorChanged: y = ${ ((Math.toDegrees(event.values[1].toDouble()) + 360) % 360)}")
+                Log.i("miladTestInterface", "onSensorChanged: z = ${ ((Math.toDegrees(event.values[2].toDouble()) + 360) % 360)}")
+
             }
             updateOrientationAngles()
         }
     }
 
+    /**
+          orientation[0] = Azimuth (rotation around the -ve z-axis)
+          orientation[1] = Pitch (rotation around the x-axis)
+          orientation[2] = Roll (rotation around the y-axis)
+       */
     fun updateOrientationAngles() {
 //            // 1
         SensorManager.getRotationMatrix(
--- app/src/main/java/com/kavinduchamiran/sensorreader/MainActivity.kt
+++ app/src/main/java/com/kavinduchamiran/sensorreader/MainActivity.kt
@@ -16,16 +16,7 @@ import androidx.appcompat.app.AppCompatActivity
 import java.lang.Math.round
 import kotlin.math.roundToInt
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
-    private lateinit var mSensorManager: SensorManager
-    private var mAccelerometer: Sensor? = null
-    private var mLight: Sensor? = null
-    private var mGyroscope: Sensor? = null
-    private var resume = false
-    private val rotationMatrix = FloatArray(9)
-    private val orientationAngles = FloatArray(3)
-    private val accelerometerReading = FloatArray(3)
-    private val magnetometerReading = FloatArray(3)
+class MainActivity : AppCompatActivity(), DirectionFinder {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -35,117 +26,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-
         setContentView(R.layout.activity_main)
-
-        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-
-        mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
-            mSensorManager.registerListener(
-                this,
-                accelerometer,
-                SensorManager.SENSOR_DELAY_NORMAL,
-                SensorManager.SENSOR_DELAY_UI
-            )
-        }
-        mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
-            mSensorManager.registerListener(
-                this,
-                magneticField,
-                SensorManager.SENSOR_DELAY_NORMAL,
-                SensorManager.SENSOR_DELAY_UI
-            )
-        }
-            mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
-            mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        print("accuracy changed")
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event != null && resume) {
-            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-                findViewById<TextView>(R.id.acc_X).text = event.values[0].toString()
-                findViewById<TextView>(R.id.acc_Y).text = event.values[1].toString()
-                findViewById<TextView>(R.id.acc_Z).text = event.values[2].toString()
-                System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
-            }
-            if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
-                System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
-            }
-            if (event.sensor.type == Sensor.TYPE_LIGHT) {
-                findViewById<TextView>(R.id.light).text = event.values[0].toString()
-            }
-
-            if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
-                findViewById<TextView>(R.id.gyro_x).text =
-                    ((Math.toDegrees(event.values[0].toDouble()) + 360) % 360).toString()
-                findViewById<TextView>(R.id.gyro_y).text =
-                    ((Math.toDegrees(event.values[1].toDouble()) + 360) % 360).toString()
-                findViewById<TextView>(R.id.gyro_z).text =
-                    ((Math.toDegrees(event.values[2].toDouble()) + 360) % 360).toString()
-            }
-            updateOrientationAngles()
-        }
-    }
-
-    /*
-       orientation[0] = Azimuth (rotation around the -ve z-axis)
-       orientation[1] = Pitch (rotation around the x-axis)
-       orientation[2] = Roll (rotation around the y-axis)
-    */
-    private fun updateOrientationAngles() {
-        // 1
-        SensorManager.getRotationMatrix(
-            rotationMatrix,
-            null,
-            accelerometerReading,
-            magnetometerReading
-        )
-        // 2
-        val orientation = SensorManager.getOrientation(rotationMatrix, orientationAngles)
-        // 3
-        val degrees = (Math.toDegrees(orientation[0].toDouble()) + 360.0) % 360.0
-        // 4
-        val angle = (degrees * 100).roundToInt() / 100
-
-        val direction = getDirection(degrees)
-
-        Log.i("miladTestOrientation", "updateOrientationAngles: orientation: $orientation")
-        Log.i("miladTestOrientation", "updateOrientationAngles: degrees: $degrees")
-        Log.i("miladTestOrientation", "updateOrientationAngles: angle: $angle")
-        Log.i("miladTestOrientation", "updateOrientationAngles: direction: $direction")
-    }
-
-    private fun getDirection(angle: Double): Direction {
-        var direction: Direction = Direction.N
-
-        if (angle >= 350 || angle <= 10)
-            direction = Direction.N
-        if (angle < 350 && angle > 280)
-            direction = Direction.NW
-        if (angle <= 280 && angle > 260)
-            direction = Direction.W
-        if (angle <= 260 && angle > 190)
-            direction = Direction.SW
-        if (angle <= 190 && angle > 170)
-            direction = Direction.S
-        if (angle <= 170 && angle > 100)
-            direction = Direction.SE
-        if (angle <= 100 && angle > 80)
-            direction = Direction.E
-        if (angle <= 80 && angle > 10)
-            direction = Direction.NE
-
-        return direction
     }
 
 
     override fun onResume() {
         super.onResume()
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(this, mMagnet, SensorManager.SENSOR_DELAY_NORMAL)
         mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL)
         mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL)
     }
@@ -162,4 +50,37 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     fun pauseReading(view: View) {
         this.resume = false
     }
+
+    override var mSensorManager: SensorManager
+        get() = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        set(value) {}
+    override var mAccelerometer: Sensor?
+        get() = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            mSensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+        set(value) {}
+    override var mMagnet: Sensor?
+        get() = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
+            mSensorManager.registerListener(
+                this,
+                magneticField,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+        set(value) {}
+    override var mLight: Sensor
+        get() = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        set(value) {}
+    override var mGyroscope: Sensor
+        get() = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        set(value) {}
+    override var resume: Boolean
+        get() = false
+       set(value) {}
 }
